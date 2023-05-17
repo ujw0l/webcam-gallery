ca2eddf723363ddc425ed4037458dc39efbcdc23
unknown <bktujwol@gmail.com>
2023-05-17 14:18:05 +0545

    Latest

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd5dd3589a067f06f8c8e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cd3bff5a5c935d16f2a9');
